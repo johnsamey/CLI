b9dbbd1ae1ce02716aa04c38b392c88a807f013c
john samy <johnsamyut@gmail.com>
2023-10-27 21:53:51 +0200

    initial

--- src/Terminal.java
+++ src/Terminal.java
@@ -9,23 +9,57 @@ public class Terminal {
     public Parser getParser() {
         return parser;
     }
+    public void echo(String[] args) {
+        System.out.println(String.join(" ", args));
+    }
     public String pwd() {
         return "Current directory: " + System.getProperty("user.dir");
     }
 
+    //john
     public void cd(String[] args) {
     }
+    //abdo ls and ls -r
+    public void ls(){
 
-    public void echo(String[] args) {
-        System.out.println(String.join(" ", args));
     }
+    //john
+    public void mkdir(){
+
+    }
+    //abdo
+    public void rmdir(){
+
+    }
+    //john
+    public void touch(){
+
+    }
+    //john cp and cp -r
+    public void cp(){
+
+    }
+    //john
+    public void rm(){
+
+    }
+    //abdo
+    public void cat(){
+
+    }
+    //abdo
+    public void wc(){
+
+    }
+
+
     public void chooseCommandAction() {
         String commandName = parser.getCommandName();
         String[] args = parser.getArgs();
 
         switch (commandName) {
             case "pwd":
-                (pwd());
+                pwd();
                 break;
             case "cd":
                 cd(args);
