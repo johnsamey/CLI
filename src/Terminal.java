d8dd94b5bfcefa5a17392135abe3d0da61efea88
abdelrahman atef <102483897+AbdoMan19@users.noreply.github.com>
2023-10-27 23:51:08 +0200

    test

--- src/Terminal.java
+++ src/Terminal.java
@@ -49,21 +49,7 @@ public class Terminal {
         }
     }
     //abdo ls and ls -r
-    public void ls(String[] args){
-        Path currentPath = Paths.get("").toAbsolutePath();
-        File currentDirectory = new File(currentPath.toString());
-        File[] files = currentDirectory.listFiles();
-        if(args.length > 0 && "-r".equals(args[0])){
-            Arrays.sort(files, (f1, f2) -> f2.getName().compareTo(f1.getName()));
-        }else{
-            Arrays.sort(files);
-        }
-        for (File file : files) {
-                System.out.println(file.getName());
-        }
-        
-
-    }
+    public void ls(String[] args){}
     //john
     public void mkdir(){
 
